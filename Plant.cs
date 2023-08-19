543e85ced7e41e08150cb9774998ab57adf29e1a
Kenny Clayton <kenny.d.clayton@gmail.com>
2023-08-19 11:49:00 -0400

    first push

---
+++ Plant.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+
+// From ChatGPT: "Each class defines its own set of properties, methods, and behavior, which you can use to create instances and manipulate data. Classes allow you to model and organize your code in an object-oriented manner."
+public class Plant 
+{
+    public string Species { get; set; } //the type here is a string
+    public int LightNeeds { get; set; } // the type here is an integer
+    public decimal AskingPrice { get; set; } // the type here is a decimal. it is more precise than double, but takes more memory and is slower.
+    public string City { get; set; } // this data type is a string
+    public int ZIP { get; set; } // this data type is an integer
+    public bool Sold { get; set; } // this data type is a boolean (true or false)
+}
+
